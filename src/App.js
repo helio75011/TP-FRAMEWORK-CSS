dd27f3b02a9401323bc84abde4267be55c057040
de Breyne <111784301+helio75011@users.noreply.github.com>
2024-02-26 12:49:30 +0100

    finish 1 step for the salle and kitchen

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@
 
 import React, { useState } from 'react';
 import CategoryFilter from './components/Categories';
-import UserList from './components/UserList';
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState('salle');
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="px-4 py-2 bg-green-500 text-white">
-        <p>Jeudi, 21 avr</p>
-        <h2>Qui travaille aujourd'hui?</h2>
+    <div className="bg-white shadow rounded-lg p-6">
+      <div className="text-center p-4">
+        <h2 className="text-lg font-bold">Qui travaille aujourd'hui ?</h2>
       </div>
       <CategoryFilter selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-      <UserList selectedCategory={selectedCategory} />
-      <button className="bg-pink-300 p-4 rounded-full text-white font-semibold self-center mb-4 shadow-lg">
-        Suivant →
-      </button>
     </div>
   );
 };
